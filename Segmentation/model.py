e41d8c83b16b9fe75f280a50d5f3ca15d018413a
wang jimmy <jimcurrywang@gmail.com>
2021-05-06 19:48:58 +0200

    add U-Net

--- Segmentation/model.py
+++ Segmentation/model.py
@@ -26,17 +26,25 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class UNET(nn.Module):
-    '''
+class UNet(nn.Module):
+    '''U-Net architecture usually uses 3 channel input size for RGB image processing 
+    and 1 channel output size
+
         ::in_channels=3
             image input with 3 channel(R-G-B)
         ::out_channels=1
             Output would be binary, so set out_channels= 1 as default
         ::features=[64, 128, 256, 512]
             the number of feature map in each block
+
+        ::torch.cat((x1, x2), dim=1)
+            https://pytorch.org/docs/stable/generated/torch.cat.html
+            
+        ::TORCHVISION.TRANSFORMS.FUNCTIONAL.resize(img, size, interpolation)
+            https://pytorch.org/vision/master/_modules/torchvision/transforms/functional.html
     '''
     def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
-        super(UNET, self).__init__()
+        super(UNet, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -74,6 +82,7 @@ class UNET(nn.Module):
         )
 
     def forward(self, x):
+        # --- downsampling ---
         # forward through the downsample part and save each output for skip_connections
         skip_connections = []
         for down in self.downs:
@@ -81,43 +90,55 @@ class UNET(nn.Module):
             skip_connections.append(x)
             x = self.pool(x)
 
+        # --- bottleneck ---
         x = self.bottleneck(x)
 
-        # reversed the skip_connections list, list(reversed(skip_connections))
+        # reversed the skip_connections list, x[::-1] == list(reversed(x))
         skip_connections = skip_connections[::-1]
 
+        # --- upsampling ---
         # for i in range(0,8,2): -> [0,1,2,3,4,5,6,7] -> 0,2,4,6
         for idx in range(0, len(self.ups), 2):
 
             # fetch ConvTranspose2d layers
             x = self.ups[idx](x)
-            # Do floot division to get the corresponding skip_connection 
+            # do floot division to get the corresponding skip_connection 
             # 0,2,4,6 -> 0,1,2,3
             skip_connection = skip_connections[idx//2]
 
+            # In the original paper, the authors use cropping to solve the size issues
+            # But resize, add padding, or consider the input be even is all suitable in here
             if x.shape != skip_connection.shape:
+                # resize by transpolation
+                # torch.Size([3, 512, 20, 20]) -> torch.Size([20, 20])
                 x = TF.resize(x, size=skip_connection.shape[2:])
 
             # channel-wise dimension feature concat
+            # dim=1 -> along the channel dimension, not on dim=0 which will increase dimension
             concat_skip = torch.cat((skip_connection, x), dim=1)
+
+            # throw the concat layer into DoubleConv
             x = self.ups[idx+1](concat_skip)
 
-        return self.final_conv(x)
+        # --- final output part ---
+        x = self.final_conv(x)
+        
+        return x
 
-def test():
-    x = torch.randn((3, 1, 161, 161))
-    model = UNET(in_channels=1, out_channels=1)
+
+def unittest():
+    '''
+        batch_size = 3 
+        channel_size = 1 
+        kernel_size = 572x572
+    '''
+    x = torch.randn((1, 3, 572, 572))
+    model = UNet(in_channels=3, out_channels=1)
     preds = model(x)
-    assert preds.shape == x.shape , (preds.shape, x.shape) 
+    print(preds.shape)
+    
+    assert preds.shape[2:] == x.shape[2:] , (preds.shape, x.shape) 
 
 if __name__ == "__main__":
-    test()
-
-    # double_conv = DoubleConv(in_channels=512, out_channels=338)
-    unet = UNET()
-    # print(unet.downs)
-
-
-
-
+    unittest()
 
