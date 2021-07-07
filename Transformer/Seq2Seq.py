22f5ca5ccc02fb672aff78f2c42b9d69889069cc
wang jimmy <jimcurrywang@gmail.com>
2021-07-07 02:12:20 +0200

    add Seq2Seq

--- Transformer/Seq2Seq.py
+++ Transformer/Seq2Seq.py
@@ -1,13 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchtext.datasets import Multi30k
-from torchtext.data import Field, BucketIterator
-import numpy as np
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
+
+import torchtext
 import spacy
+
+import numpy as np
 import random
+
 from torch.utils.tensorboard import SummaryWriter  # to print to tensorboard
-from utils import translate_sentence, bleu, save_checkpoint, load_checkpoint
+# from utils import translate_sentence, bleu, save_checkpoint, load_checkpoint
 
 '''
 Multi30K is a dataset to stimulate multilingual multimodal research for English-German. 
@@ -15,236 +19,259 @@ It is based on the Flickr30k dataset, which contains 31,014 images sourced from 
 Each image is paired with five English descriptions, which were collected from Amazon Mechanical Turk. 
 The dataset contains 145,000 training, 5,070 development, and 5,000 test descriptions. 
 The Multi30K dataset extends the Flickr30K dataset with translated and independent German sentences.
+
+
+BucketIterator
+    train_iter, val_iter = BucketIterator.splits(
+         (trn, vld), # we pass in the datasets we want the iterator to draw data from
+         batch_sizes=(64, 64),
+         device=-1, # if you want to use the GPU, specify the GPU number here
+         sort_key=lambda x: len(x.comment_text), # the BucketIterator needs to be told what function it should use to group the data.
+         sort_within_batch=False,
+         repeat=False # we pass repeat=False because we want to wrap this Iterator layer.
+    )
+
+    test_iter = Iterator(tst, batch_size=64, device=-1, sort=False, sort_within_batch=False, repeat=False)
+
+Field
+    + the "unk_token" for out-of-vocabulary words
+    + the "pad_token" for padding
+    + the "eos_token" for the end of a sentence
+    + an optional "init_token" for the start of the sentence
+
 '''
-
-spacy_ger = spacy.load("de")
-spacy_eng = spacy.load("en")
-
 
 def tokenize_ger(text):
     return [tok.text for tok in spacy_ger.tokenizer(text)]
 
-
 def tokenize_eng(text):
     return [tok.text for tok in spacy_eng.tokenizer(text)]
 
+spacy_ger = spacy.load("de_core_news_sm")
+spacy_eng = spacy.load("en_core_web_sm")
 
-german = Field(tokenize=tokenize_ger, lower=True, init_token="<sos>", eos_token="<eos>")
+german = Field(tokenize=tokenize_ger, lower=True, 
+    init_token="<sos>", eos_token="<eos>")
+english = Field(tokenize=tokenize_eng, lower=True, 
+    init_token="<sos>", eos_token="<eos>")
 
-english = Field(
-    tokenize=tokenize_eng, lower=True, init_token="<sos>", eos_token="<eos>"
-)
-
-train_data, valid_data, test_data = Multi30k.splits(
-    exts=(".de", ".en"), fields=(german, english)
-)
+train_data, valid_data, test_data = Multi30k.splits( exts=(".de", ".en"), 
+                                                fields=(german, english))
 
 german.build_vocab(train_data, max_size=10000, min_freq=2)
 english.build_vocab(train_data, max_size=10000, min_freq=2)
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size, num_layers, p):
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, dropout_rate):
         super(Encoder, self).__init__()
-        self.dropout = nn.Dropout(p)
+        
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        self.embedding = nn.Embedding(input_size, embedding_size)
-        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+        self.dropout = nn.Dropout(dropout_rate)
+        
+        self.embedding = nn.Embedding(
+            num_embeddings=input_size, embedding_dim=embedding_size
+        )
+        self.rnn = nn.LSTM(
+            input_size=embedding_size, hidden_size=hidden_size, 
+            num_layers=num_layers, dropout=dropout_rate
+        )
 
     def forward(self, x):
-        # x shape: (seq_length, N) where N is batch size
-
-        embedding = self.dropout(self.embedding(x))
-        # embedding shape: (seq_length, N, embedding_size)
-
+        '''
+            x shape: (seq_length, N) where N is batch size
+            embedding shape: (seq_length, N, embedding_size)
+            outputs shape: (seq_length, N, hidden_size)
+        '''
+        embedding = self.embedding(x)
+        embedding = self.dropout(embedding)
         outputs, (hidden, cell) = self.rnn(embedding)
-        # outputs shape: (seq_length, N, hidden_size)
 
         return hidden, cell
 
 
-class Decoder(nn.Module):
-    def __init__(
-        self, input_size, embedding_size, hidden_size, output_size, num_layers, p
-    ):
-        super(Decoder, self).__init__()
-        self.dropout = nn.Dropout(p)
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
+# class Decoder(nn.Module):
+#     def __init__(
+#         self, input_size, embedding_size, hidden_size, output_size, num_layers, p
+#     ):
+#         super(Decoder, self).__init__()
+#         self.dropout = nn.Dropout(p)
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
 
-        self.embedding = nn.Embedding(input_size, embedding_size)
-        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
-        self.fc = nn.Linear(hidden_size, output_size)
+#         self.embedding = nn.Embedding(input_size, embedding_size)
+#         self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+#         self.fc = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x, hidden, cell):
-        # x shape: (N) where N is for batch size, we want it to be (1, N), seq_length
-        # is 1 here because we are sending in a single word and not a sentence
-        x = x.unsqueeze(0)
+#     def forward(self, x, hidden, cell):
+#         # x shape: (N) where N is for batch size, we want it to be (1, N), seq_length
+#         # is 1 here because we are sending in a single word and not a sentence
+#         x = x.unsqueeze(0)
 
-        embedding = self.dropout(self.embedding(x))
-        # embedding shape: (1, N, embedding_size)
+#         embedding = self.dropout(self.embedding(x))
+#         # embedding shape: (1, N, embedding_size)
 
-        outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
-        # outputs shape: (1, N, hidden_size)
+#         outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
+#         # outputs shape: (1, N, hidden_size)
 
-        predictions = self.fc(outputs)
+#         predictions = self.fc(outputs)
 
-        # predictions shape: (1, N, length_target_vocabulary) to send it to
-        # loss function we want it to be (N, length_target_vocabulary) so we're
-        # just gonna remove the first dim
-        predictions = predictions.squeeze(0)
+#         # predictions shape: (1, N, length_target_vocabulary) to send it to
+#         # loss function we want it to be (N, length_target_vocabulary) so we're
+#         # just gonna remove the first dim
+#         predictions = predictions.squeeze(0)
 
-        return predictions, hidden, cell
-
-
-class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder):
-        super(Seq2Seq, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def forward(self, source, target, teacher_force_ratio=0.5):
-        batch_size = source.shape[1]
-        target_len = target.shape[0]
-        target_vocab_size = len(english.vocab)
-
-        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
-
-        hidden, cell = self.encoder(source)
-
-        # Grab the first input to the Decoder which will be <SOS> token
-        x = target[0]
-
-        for t in range(1, target_len):
-            # Use previous hidden, cell as context from encoder at start
-            output, hidden, cell = self.decoder(x, hidden, cell)
-
-            # Store next output prediction
-            outputs[t] = output
-
-            # Get the best word the Decoder predicted (index in the vocabulary)
-            best_guess = output.argmax(1)
-
-            # With probability of teacher_force_ratio we take the actual next word
-            # otherwise we take the word that the Decoder predicted it to be.
-            # Teacher Forcing is used so that the model gets used to seeing
-            # similar inputs at training and testing time, if teacher forcing is 1
-            # then inputs at test time might be completely different than what the
-            # network is used to. This was a long comment.
-            x = target[t] if random.random() < teacher_force_ratio else best_guess
-
-        return outputs
+#         return predictions, hidden, cell
 
 
-### We're ready to define everything we need for training our Seq2Seq model ###
+# class Seq2Seq(nn.Module):
+#     def __init__(self, encoder, decoder):
+#         super(Seq2Seq, self).__init__()
+#         self.encoder = encoder
+#         self.decoder = decoder
 
-# Training hyperparameters
-num_epochs = 100
-learning_rate = 0.001
-batch_size = 64
+#     def forward(self, source, target, teacher_force_ratio=0.5):
+#         batch_size = source.shape[1]
+#         target_len = target.shape[0]
+#         target_vocab_size = len(english.vocab)
 
-# Model hyperparameters
-load_model = False
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-input_size_encoder = len(german.vocab)
-input_size_decoder = len(english.vocab)
-output_size = len(english.vocab)
-encoder_embedding_size = 300
-decoder_embedding_size = 300
-hidden_size = 1024  # Needs to be the same for both RNN's
-num_layers = 2
-enc_dropout = 0.5
-dec_dropout = 0.5
+#         outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
 
-# Tensorboard to get nice loss plot
-writer = SummaryWriter(f"runs/loss_plot")
-step = 0
+#         hidden, cell = self.encoder(source)
 
-train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
-    (train_data, valid_data, test_data),
-    batch_size=batch_size,
-    sort_within_batch=True,
-    sort_key=lambda x: len(x.src),
-    device=device,
-)
+#         # Grab the first input to the Decoder which will be <SOS> token
+#         x = target[0]
 
-encoder_net = Encoder(
-    input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout
-).to(device)
+#         for t in range(1, target_len):
+#             # Use previous hidden, cell as context from encoder at start
+#             output, hidden, cell = self.decoder(x, hidden, cell)
 
-decoder_net = Decoder(
-    input_size_decoder,
-    decoder_embedding_size,
-    hidden_size,
-    output_size,
-    num_layers,
-    dec_dropout,
-).to(device)
+#             # Store next output prediction
+#             outputs[t] = output
 
-model = Seq2Seq(encoder_net, decoder_net).to(device)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+#             # Get the best word the Decoder predicted (index in the vocabulary)
+#             best_guess = output.argmax(1)
 
-pad_idx = english.vocab.stoi["<pad>"]
-criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+#             # With probability of teacher_force_ratio we take the actual next word
+#             # otherwise we take the word that the Decoder predicted it to be.
+#             # Teacher Forcing is used so that the model gets used to seeing
+#             # similar inputs at training and testing time, if teacher forcing is 1
+#             # then inputs at test time might be completely different than what the
+#             # network is used to. This was a long comment.
+#             x = target[t] if random.random() < teacher_force_ratio else best_guess
 
-if load_model:
-    load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+#         return outputs
 
 
-sentence = "ein boot mit mehreren männern darauf wird von einem großen pferdegespann ans ufer gezogen."
+# ### We're ready to define everything we need for training our Seq2Seq model ###
 
-for epoch in range(num_epochs):
-    print(f"[Epoch {epoch} / {num_epochs}]")
+# # Training hyperparameters
+# num_epochs = 100
+# learning_rate = 0.001
+# batch_size = 64
 
-    checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
-    save_checkpoint(checkpoint)
+# # Model hyperparameters
+# load_model = False
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# input_size_encoder = len(german.vocab)
+# input_size_decoder = len(english.vocab)
+# output_size = len(english.vocab)
+# encoder_embedding_size = 300
+# decoder_embedding_size = 300
+# hidden_size = 1024  # Needs to be the same for both RNN's
+# num_layers = 2
+# enc_dropout = 0.5
+# dec_dropout = 0.5
 
-    model.eval()
+# # Tensorboard to get nice loss plot
+# writer = SummaryWriter(f"runs/loss_plot")
+# step = 0
 
-    translated_sentence = translate_sentence(
-        model, sentence, german, english, device, max_length=50
-    )
+# train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+#     (train_data, valid_data, test_data), # # we pass in the datasets we want the iterator to draw data from
+#     batch_size=batch_size,
+#     sort_within_batch=True,
+#     sort_key=lambda x: len(x.src),  # the BucketIterator needs to be told what function it should use to group the data.
+#     device=device, # if you want to use the GPU, specify the GPU number here
+# )
 
-    print(f"Translated example sentence: \n {translated_sentence}")
+# encoder_net = Encoder(
+#     input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout
+# ).to(device)
 
-    model.train()
+# decoder_net = Decoder(
+#     input_size_decoder,
+#     decoder_embedding_size,
+#     hidden_size,
+#     output_size,
+#     num_layers,
+#     dec_dropout,
+# ).to(device)
 
-    for batch_idx, batch in enumerate(train_iterator):
-        # Get input and targets and get to cuda
-        inp_data = batch.src.to(device)
-        target = batch.trg.to(device)
+# model = Seq2Seq(encoder_net, decoder_net).to(device)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-        # Forward prop
-        output = model(inp_data, target)
+# pad_idx = english.vocab.stoi["<pad>"]
+# criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
-        # Output is of shape (trg_len, batch_size, output_dim) but Cross Entropy Loss
-        # doesn't take input in that form. For example if we have MNIST we want to have
-        # output to be: (N, 10) and targets just (N). Here we can view it in a similar
-        # way that we have output_words * batch_size that we want to send in into
-        # our cost function, so we need to do some reshapin. While we're at it
-        # Let's also remove the start token while we're at it
-        output = output[1:].reshape(-1, output.shape[2])
-        target = target[1:].reshape(-1)
-
-        optimizer.zero_grad()
-        loss = criterion(output, target)
-
-        # Back prop
-        loss.backward()
-
-        # Clip to avoid exploding gradient issues, makes sure grads are
-        # within a healthy range
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
-
-        # Gradient descent step
-        optimizer.step()
-
-        # Plot to tensorboard
-        writer.add_scalar("Training loss", loss, global_step=step)
-        step += 1
+# if load_model:
+#     load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
 
 
-score = bleu(test_data[1:100], model, german, english, device)
-print(f"Bleu score {score*100:.2f}")
+# sentence = "ein boot mit mehreren männern darauf wird von einem großen pferdegespann ans ufer gezogen."
+
+# for epoch in range(num_epochs):
+#     print(f"[Epoch {epoch} / {num_epochs}]")
+
+#     checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
+#     save_checkpoint(checkpoint)
+
+#     model.eval()
+
+#     translated_sentence = translate_sentence(
+#         model, sentence, german, english, device, max_length=50
+#     )
+
+#     print(f"Translated example sentence: \n {translated_sentence}")
+
+#     model.train()
+
+#     for batch_idx, batch in enumerate(train_iterator):
+#         # Get input and targets and get to cuda
+#         inp_data = batch.src.to(device)
+#         target = batch.trg.to(device)
+
+#         # Forward prop
+#         output = model(inp_data, target)
+
+#         # Output is of shape (trg_len, batch_size, output_dim) but Cross Entropy Loss
+#         # doesn't take input in that form. For example if we have MNIST we want to have
+#         # output to be: (N, 10) and targets just (N). Here we can view it in a similar
+#         # way that we have output_words * batch_size that we want to send in into
+#         # our cost function, so we need to do some reshapin. While we're at it
+#         # Let's also remove the start token while we're at it
+#         output = output[1:].reshape(-1, output.shape[2])
+#         target = target[1:].reshape(-1)
+
+#         optimizer.zero_grad()
+#         loss = criterion(output, target)
+
+#         # Back prop
+#         loss.backward()
+
+#         # Clip to avoid exploding gradient issues, makes sure grads are
+#         # within a healthy range
+#         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+
+#         # Gradient descent step
+#         optimizer.step()
+
+#         # Plot to tensorboard
+#         writer.add_scalar("Training loss", loss, global_step=step)
+#         step += 1
+
+
+# score = bleu(test_data[1:100], model, german, english, device)
+# print(f"Bleu score {score*100:.2f}")
